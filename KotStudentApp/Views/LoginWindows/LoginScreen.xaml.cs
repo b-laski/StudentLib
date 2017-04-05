43260c2f1a6293c7ca96329a2506d10e4185dc1c
DESKTOP-4D94KU7\Tajfun695 <tajfun695@gmail.com>
2017-04-05 22:22:41 +0200

    Update

--- KotStudentApp/Views/LoginWindows/LoginScreen.xaml.cs
+++ KotStudentApp/Views/LoginWindows/LoginScreen.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace KotStudentApp.Views.LoginWindows
 {
@@ -23,6 +13,30 @@ namespace KotStudentApp.Views.LoginWindows
         public LoginScreen()
         {
             InitializeComponent();
+        }
+
+        public void StartingAnimation()
+        {
+            EllipseGeometry geometry = new EllipseGeometry(new Point(this.ActualWidth / 2, this.ActualHeight / 2), 0, 0);
+            this.Width = ActualWidth;
+            this.Height = ActualHeight;
+
+            int EllipseFinalWidth = (int) Utilities.MathFunctions.CalculatePitagoras(this.ActualWidth / 2, this.ActualHeight / 2);
+            this.Clip = geometry;
+            grdContent.Opacity = 0;
+
+            Utilities.Animations.RadiusXAnimation(geometry, EllipseFinalWidth, TimeSpan.FromMilliseconds(600));
+            Utilities.Animations.RadiusYAnimation(geometry, EllipseFinalWidth, TimeSpan.FromMilliseconds(600), ()=>
+            {
+                this.Clip = null;
+                Utilities.Animations.OpacityAnimation(grdContent, 1, TimeSpan.FromMilliseconds(600));
+            });
+
+        }
+
+        private void MainParent_Loaded(object sender, RoutedEventArgs e)
+        {
+            StartingAnimation();
         }
     }
 }
